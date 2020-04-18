5ebabdb67d48473aebf61221cdb2f927076a90f0
Jeff Wong <v-jwong1@expedia.com>
2020-04-18 17:54:46 +1000

    generate getters

--- src/main/java/com/pluralsight/model/Speaker.java
+++ src/main/java/com/pluralsight/model/Speaker.java
@@ -3,4 +3,11 @@ package com.pluralsight.model;
 public class Speaker {
     private String firstName;
     private String lastName;
+
+    public String getFirstName() {
+        return firstName;
+    }
+    public String getLastName() {
+        return lastName;
+    }
 }
