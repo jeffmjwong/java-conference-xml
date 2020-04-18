bb7bdcb5fc91d389dc92042ecc9861071ca950b1
Jeff Wong <v-jwong1@expedia.com>
2020-04-18 17:55:14 +1000

    generate setters

--- src/main/java/com/pluralsight/model/Speaker.java
+++ src/main/java/com/pluralsight/model/Speaker.java
@@ -7,7 +7,14 @@ public class Speaker {
     public String getFirstName() {
         return firstName;
     }
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
     public String getLastName() {
         return lastName;
+    }
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
     }
 }
