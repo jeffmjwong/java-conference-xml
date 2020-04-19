05225512abf0416ab85c447827539d133e04b0a3
Jeff Wong <v-jwong1@expedia.com>
2020-04-19 12:16:05 +1000

    implement findAll method in HibernateSpeakerRepositoryImpl

--- src/main/java/com/pluralsight/repository/HibernateSpeakerRepositoryImpl.java
+++ src/main/java/com/pluralsight/repository/HibernateSpeakerRepositoryImpl.java
@@ -1,4 +1,20 @@
 package com.pluralsight.repository;
 
+import com.pluralsight.model.Speaker;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class HibernateSpeakerRepositoryImpl {
+    public List<Speaker> findAll() {
+        final List<Speaker> speakers = new ArrayList<>();
+
+        final Speaker speaker = new Speaker();
+        speaker.setFirstName("Bryan");
+        speaker.setLastName("Hansen");
+
+        speakers.add(speaker);
+
+        return speakers;
+    }
 }
