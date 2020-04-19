c6b11e0bfa1c8457e2ac4f9cf9aad6b5197c6188
Jeff Wong <v-jwong1@expedia.com>
2020-04-19 13:13:03 +1000

    finish up Application class

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,2 +1,9 @@
+import com.pluralsight.service.SpeakerService;
+import com.pluralsight.service.SpeakerServiceImpl;
+
 public class Application {
+    public static void main (String[] args) {
+        final SpeakerService service = new SpeakerServiceImpl();
+        System.out.println(service.findAll().get(0).getFirstName());
+    }
 }
