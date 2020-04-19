dff4dd1157ebef18de7da4c414628a9a02e59ace
Jeff Wong <v-jwong1@expedia.com>
2020-04-19 12:57:00 +1000

    create findAll method and private SpeakerRepository field in SpeakerServiceImpl

--- src/main/java/com/pluralsight/service/SpeakerServiceImpl.java
+++ src/main/java/com/pluralsight/service/SpeakerServiceImpl.java
@@ -1,4 +1,15 @@
 package com.pluralsight.service;
 
+import com.pluralsight.model.Speaker;
+import com.pluralsight.repository.HibernateSpeakerRepositoryImpl;
+import com.pluralsight.repository.SpeakerRepository;
+
+import java.util.List;
+
 public class SpeakerServiceImpl {
+    private final SpeakerRepository repository = new HibernateSpeakerRepositoryImpl();
+
+    public List<Speaker> findAll() {
+        return repository.findAll();
+    }
 }
