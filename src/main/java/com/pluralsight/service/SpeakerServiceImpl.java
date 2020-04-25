296d071ac7ebc308136bcf872fc3d93f7a60a297
Jeff Wong <v-jwong1@expedia.com>
2020-04-25 16:29:53 +1000

    autowired by name

--- src/main/java/com/pluralsight/service/SpeakerServiceImpl.java
+++ src/main/java/com/pluralsight/service/SpeakerServiceImpl.java
@@ -8,9 +8,13 @@ import java.util.List;
 public class SpeakerServiceImpl implements SpeakerService {
     private SpeakerRepository repository;
 
-    public SpeakerServiceImpl() {}
+//    public SpeakerServiceImpl() {}
+//
+//    public SpeakerServiceImpl(SpeakerRepository repository) {
+//        this.repository = repository;
+//    }
 
-    public SpeakerServiceImpl(SpeakerRepository repository) {
+    public void setSpeakerRepository(SpeakerRepository repository) {
         this.repository = repository;
     }
 
